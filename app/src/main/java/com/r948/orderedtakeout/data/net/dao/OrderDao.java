0a67211a9bfb7c0d623e25353cdb3f9896f43ff3
songzihao <18234404647@163.com>
2021-07-21 10:33:57 +0800

    3个dao

--- app/src/main/java/com/r948/orderedtakeout/data/net/dao/OrderDao.java
+++ app/src/main/java/com/r948/orderedtakeout/data/net/dao/OrderDao.java
@@ -1,4 +1,19 @@
 package com.r948.orderedtakeout.data.net.dao;
 
+import com.r948.orderedtakeout.data.model.Order;
+import com.r948.orderedtakeout.data.model.OrderCommodity;
+
 public interface OrderDao {
+
+    // select * from order where user_id = :userid
+    Order[] findOrderByUserId(int userid);
+
+    // select * from order where deliver_id = :deliverId
+    Order[] findOrderByDeliverId(int deliverId);
+
+    // insert
+    int addOrder(Order order);
+
+    // delete
+    int deleteOrder(Order order);
 }
