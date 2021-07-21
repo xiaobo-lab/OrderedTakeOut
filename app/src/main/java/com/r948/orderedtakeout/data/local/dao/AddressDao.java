8df2d6b77048b4ac3851f8b0a9fa463a8bbb9ae9
Drk <1211154622@qq.com>
2021-07-21 09:15:52 +0800

    shopping_cart.xml添加

--- app/src/main/java/com/r948/orderedtakeout/data/local/dao/AddressDao.java
+++ app/src/main/java/com/r948/orderedtakeout/data/local/dao/AddressDao.java
@@ -1,4 +1,8 @@
 package com.r948.orderedtakeout.data.local.dao;
 
+import com.r948.orderedtakeout.data.model.Address;
+
 public interface AddressDao {
+    //select * from address ,user_address where address.user_id =: userId and user_address.address_id = address.address_id
+    Address[] findAddressByUserId(Integer userId);
 }
